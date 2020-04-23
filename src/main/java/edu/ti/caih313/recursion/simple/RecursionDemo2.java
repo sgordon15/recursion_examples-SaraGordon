384323070956b6769157458671c990657143826f
cliberman <chanaliberman1@gmail.com>
2020-04-23 16:34:50 -0500

    lab 7 chana liberman recursion countdown and demo 2

--- src/main/java/edu/ti/caih313/recursion/simple/RecursionDemo2.java
+++ src/main/java/edu/ti/caih313/recursion/simple/RecursionDemo2.java
@@ -8,7 +8,7 @@ public class RecursionDemo2 {
         Scanner keyboard = new Scanner(System.in);
         int number = keyboard.nextInt();
         System.out.println(number + " contains " +
-                getNumberOfZeros(number) + " zeros.");
+                getNumberOfZeros(number, ">") + " zeros.");
     }
 
 
@@ -16,16 +16,24 @@ public class RecursionDemo2 {
      * Precondition: n >= 0
      * Returns the number of zero digits in n.
      */
-    public static int getNumberOfZeros(int n) {
+    public static int getNumberOfZeros(int n, String prefix) {
+        System.out.println("Entering getNumberOfZeros with " + n);
         int result;
-        if (n == 0)
+        if (n == 0) {
+            System.out.println(prefix + "setting result to 1, because n == 0");
             result = 1;
-        else if (n < 10)
+        } else if (n < 10) {
+            System.out.println(prefix + "setting result to 0, because n is " + n);
             result = 0; //n has one digit that is not 0
-        else if (n % 10 == 0)
-            result = getNumberOfZeros(n / 10) + 1;
-        else //n % 10 != 0
-            result = getNumberOfZeros(n / 10);
+        } else if (n % 10 == 0) {
+            System.out.println(prefix + "found another zero");
+            System.out.println(prefix + "calling getNumberOfZeros with" + (n/10));
+            result = getNumberOfZeros(n / 10, "-" + prefix) + 1;
+        } else {//n % 10 != 0
+            System.out.println(prefix + "calling getNumberOfZeros with" + (n/10));
+            result = getNumberOfZeros(n / 10, "-" + prefix);
+        }
+        System.out.println("Exiting getNumberOfZeros with " + n);
         return result;
     }
 }
--- src/main/java/edu/ti/caih313/recursion/simple/RecursiveCountdown.java
+++ src/main/java/edu/ti/caih313/recursion/simple/RecursiveCountdown.java
@@ -2,15 +2,18 @@ package edu.ti.caih313.recursion.simple;
 
 public class RecursiveCountdown {
     public static void main(String[] args) {
-        countDown(3);
+        countDown(3, ">");
     }
 
-    public static void countDown(int num) {
+    public static void countDown(int num, String prefix) {
+        System.out.println("Entering countdown with " + num);
         if (num == 0) {
-            System.out.println("That's all, folks!");
+            //System.out.println("That's all, folks!");
+            System.out.println(prefix + "Done!");
         } else {
-            System.out.print(num + " ");
-            countDown(num - 1);
+            System.out.println(prefix + "Calling countdown with " + (num - 1));
+            countDown(num - 1, "-" + prefix);
         }
+        System.out.println(prefix + "Exiting countdown with " + num);
     }
 }
